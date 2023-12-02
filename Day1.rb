98dea6915bef9e7789b7b28ded074e2e8e44d080
jcrispinroundtree <crispinroundtree@gmail.com>
2023-12-02 17:48:53 +0000

    Day 1 Part 2 Complete!

--- Day1.rb
+++ Day1.rb
@@ -1,17 +1,12 @@
-input = File.read('input.txt')
-
-def parser(input)
-    total = 0
-    input.each_line do |row|
-        row = row.gsub(/[a-zA-Z\s]/, '')
-        next if row.empty?
-        first_digit = row[0]
-        last_digit = row[-1]
-        tempnumber = "#{first_digit}#{last_digit}"
-        puts tempnumber
-        total += tempnumber.to_i
-    end
-    return total
+input = File.readlines('input.txt').map(&:chomp)
+total = 0
+digit_map = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+digit_map_reversed = digit_map.map(&:reverse)
+input.each do |line|
+  positions = (1..9).map { |i| [line.index(i.to_s), i.to_s] } + digit_map.each_with_index.map { |number_string, i| [line.index(number_string), (i + 1).to_s] }
+  first_digit = positions.reject { |position| position[0].nil? }.min_by(&:first)[1]
+  positions = (1..9).map { |i| [line.reverse.index(i.to_s), i.to_s] } + digit_map_reversed.each_with_index.map { |number_string, i| [line.reverse.index(number_string), (i + 1).to_s] }
+  last_digit = positions.reject { |position| position[0].nil? }.min_by(&:first)[1]
+  total += (first_digit + last_digit).to_i
 end
-  
-puts parser(input)
+puts total
