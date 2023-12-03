212159eca996cf743be7c6819728c7c996a93fdb
jcrispinroundtree <crispinroundtree@gmail.com>
2023-12-03 11:08:49 +0000

    Day 2 Part 2 Complete!

--- Day2.rb
+++ Day2.rb
@@ -1,4 +1,5 @@
 input = File.readlines('Day2-input.txt').map(&:chomp)
+# PART 1
 total = 0
 input.each do |game|
     game_valid = true
@@ -17,3 +18,29 @@ input.each do |game|
     total += gameid if game_valid
 end
 puts total
+# PART 2
+total2 = 0
+input.each do |game|
+    game_name, game_data = game.split(': ', 2) 
+    game_sections = game.split(';').map(&:strip)
+    red_max = 0
+    green_max = 0
+    blue_max = 0
+    game_sections.each do |section|
+        red_count = section.scan(/(\d+) red/).flatten.map(&:to_i).sum
+        green_count = section.scan(/(\d+) green/).flatten.map(&:to_i).sum
+        blue_count = section.scan(/(\d+) blue/).flatten.map(&:to_i).sum
+        if red_max < red_count
+            red_max = red_count
+        end
+        if green_max < green_count
+            green_max = green_count
+        end
+        if blue_max < blue_count
+            blue_max = blue_count
+        end
+    end
+    colour_power = red_max * green_max * blue_max
+    total2 += colour_power
+end
+puts total2
