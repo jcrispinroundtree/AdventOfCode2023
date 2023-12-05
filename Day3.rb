53964c2ef3a41c089a234eb4f5dbae3c8b05743c
jcrispinroundtree <crispinroundtree@gmail.com>
2023-12-05 15:49:54 +0000

    Day 3 Part 2 Complete!

--- Day3.rb
+++ Day3.rb
@@ -51,109 +51,50 @@ def counter2(input)
     end
   end
   total = 0
-  number_str1 = ''
-  number_str2 = ''
-  foundnum1 = false
-  foundnum2 = false
+
   height.times do |i|
     width.times do |j|
       if matrix[i][j] == '*'
-        [-1, 0, 1].each do |x|
-          [-1, 0, 1].each do |y|
-            ni, nj = i + x, j + y
-            if ni.between?(0, height - 1) && nj.between?(0, width - 1)
-              if matrix[ni][nj].match?(/\d/) && foundnum1 == false && foundnum2 == false
-                if x == -1  && matrix[ni+1][nj].match(/\D/) || x == 0 && matrix[ni+1][nj].match(/\D/)
-                  number_str1 = readnum1(ni, nj, matrix, height, width)
-                  puts "number 1 is " + number_str1
-                  foundnum1 = true
-                  break
-                end
-                if x == 0 && matrix[ni+1][nj].match(/\D/)
-                  number_str1 = readnum1(ni, nj, matrix, height, width)
-                  puts "number 1 is " + number_str1
-                  foundnum1 = true
-                  break
-                end
-                if x == 1 && matrix[ni-1][nj].match(/\D/)
-                  number_str1 = readnum1(ni, nj, matrix, height, width)
-                  puts "number 1 is " + number_str1
-                  foundnum1 = true
-                  break
-                end
-              end
-              if matrix[ni][nj].match?(/\d/) && foundnum1 == true && foundnum2 == false
-                if x == -1  && matrix[ni+1][nj].match(/\D/)
-                  number_str2 = readnum2(ni, nj, matrix, height, width)
-                  puts "number 2 is " + number_str2
-                  foundnum2 = true
-                  break
-                end
-                if x == 0 && matrix[ni+1][nj].match(/\D/)
-                  number_str2 = readnum2(ni, nj, matrix, height, width)
-                  puts "number 2 is " + number_str2
-                  foundnum2 = true
-                  break
-                end
-                if x == 1 && matrix[ni-1][nj].match(/\D/)
-                  number_str2 = readnum2(ni, nj, matrix, height, width)
-                  puts "number 2 is " + number_str2
-                  foundnum2 = true
-                  break
-                end
-              end
-            end
-          end
+        numbers = find_numbers_around_star(matrix, i, j, height, width)
+        if numbers.length == 2
+          num1, num2 = numbers
+          total += num1.to_i * num2.to_i
+          puts "#{num1.to_i} times #{num2.to_i} is #{num1.to_i * num2.to_i}"
         end
-        if foundnum1 == true && foundnum2 == true
-          total += number_str1.to_i * number_str2.to_i
-          puts "#{number_str1.to_i} times #{number_str2.to_i} is #{number_str1.to_i * number_str2.to_i}"
-        end
-        foundnum1 = false
-        foundnum2 = false
       end
     end
   end     
   total
 end
 
-def readnum1( i, j, matrix, height, width)
-  ni, nj = i, j
-  c = 0
-  while matrix[ni][nj-c].match?(/\d/)
-    c += 1
-  end
-  if matrix[ni][nj-c].match?(/\D/)
-    c -= 1
-    number_str1 = matrix[ni][nj-c].to_s
-    k = (nj - c) + 1
-    while k < width && matrix[ni][k].match?(/\d/)
-      number_str1 += matrix[ni][k].to_s
-      k += 1
+def find_numbers_around_star(matrix, i, j, height, width)
+  numbers = []
+  [-1, 0, 1].each do |x|
+    [-1, 0, 1].each do |y|
+      ni, nj = i + x, j + y
+      if ni.between?(0, height - 1) && nj.between?(0, width - 1) && matrix[ni][nj].match?(/\d/)
+        number = read_number(ni, nj, matrix, height, width)
+        numbers.push(number) unless numbers.include?(number)
+      end
     end
   end
-  return number_str1
+  numbers
 end
 
-def readnum2( i, j, matrix, height, width)
-  ni, nj = i, j
-  c = 0
-  while matrix[ni][nj-c].match?(/\d/)
-    c += 1
+def read_number(i, j, matrix, height, width)
+  number_str = matrix[i][j].to_s
+  k = j - 1
+  while k >= 0 && matrix[i][k].match?(/\d/)
+    number_str = matrix[i][k].to_s + number_str
+    k -= 1
   end
-  if matrix[ni][nj-c].match?(/\D/)
-    c -= 1
-    number_str2 = matrix[ni][nj-c].to_s
-    k = (nj - c) + 1
-    while k < width && matrix[ni][k].match?(/\d/)
-      number_str2 += matrix[ni][k].to_s
-      k += 1
-    end
+  k = j + 1
+  while k < width && matrix[i][k].match?(/\d/)
+    number_str += matrix[i][k].to_s
+    k += 1
   end
-  return number_str2
+  number_str
 end
-
-
 
 puts counter2(input)
 
