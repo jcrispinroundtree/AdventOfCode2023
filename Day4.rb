8179961d6f3e4b4cceaa2b06a9930d6d833c5cfd
jcrispinroundtree <crispinroundtree@gmail.com>
2023-12-06 13:56:09 +0000

    Day 4 Part 2 Complete!

--- Day4.rb
+++ Day4.rb
@@ -1,6 +1,5 @@
-input = File.readlines('Day4-input.txt').map(&:chomp)
-
 # PART 1
+input = File.readlines('Day4-input.txt').map(&:chomp)
 total = 0
 input.each do |line|
     temp_total = 0
@@ -18,6 +17,32 @@ input.each do |line|
         end
     end
     total += temp_total
-    
 end
 puts total
+
+# PART 2
+Card = Struct.new(:card_id, :matches)
+cards = []
+total2 = 0
+input.each do |line|
+  card_id = line.match(/Card\s+(\d+):/)[1].to_i
+  matches = 0
+  _, numbers = line.split(':', 2)
+  left_numbers, right_numbers = numbers.split('|').map { |s| s.split.map(&:to_i) }
+  left_numbers.each do |l_number|
+    matches += 1 if right_numbers.include?(l_number)
+  end
+  cards << Card.new(card_id, matches)
+end
+total_cards = Hash.new(0)
+cards.each do |card|
+  total_cards[card.card_id] += 1
+  card.matches.times do |i|
+    next_card_id = card.card_id + i + 1
+    break if next_card_id > cards.size 
+    total_cards[next_card_id] += total_cards[card.card_id]
+  end
+end
+
+total2 = total_cards.values.sum
+puts total2
