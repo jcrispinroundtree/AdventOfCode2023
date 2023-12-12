982f9076567f72d1e1b0c73e15d4d3440f989a25
jcrispinroundtree <crispinroundtree@gmail.com>
2023-12-12 17:56:06 +0000

    Day 9 Part 2 Complete!

--- Day9.rb
+++ Day9.rb
@@ -2,22 +2,47 @@
 input = File.readlines('Day9-input.txt').map(&:chomp)
 total = 0
 
-def process_array(input)
+def find_next(input)
     arrays = [input]
     last_array = input
     loop do
-      new_array = last_array.each_cons(2).map { |a, b| b - a }
-      break if new_array.uniq == [0]
-      arrays << new_array
-      last_array = new_array
+        new_array = last_array.each_cons(2).map { |a, b| b - a }
+        break if new_array.uniq == [0]
+        arrays << new_array
+        last_array = new_array
     end
     arrays.map(&:last).sum
 end
 
 input.each do |line|
     line = line.split(' ').map(&:to_i)
-    result = process_array(line)
-    total += result
+    total += find_next(line)
 end
 
 puts total
+
+# PART 2
+histories = []
+total2 = 0
+
+File.open("Day9-input.txt") do |file|
+    file.each_line do |line|
+        histories << line.split.map(&:to_i)
+    end
+end
+
+histories.each do |history|
+    descents = [history]
+    while true
+        next_level = descents.last.each_cons(2).map { |a, b| b - a }
+        break if next_level.all? { |x| x == 0 }
+        descents << next_level
+    end
+    diff = 0
+    descents.reverse_each do |descent|
+        diff = descent.first - diff
+    end
+    total2 += diff
+end
+
+puts total2
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # AdventOfCode2023
 Practicing Ruby
 
-![](https://img.shields.io/badge/stars%20⭐-17-yellow)
-![](https://img.shields.io/badge/days%20completed-8-red)
+![](https://img.shields.io/badge/stars%20⭐-18-yellow)
+![](https://img.shields.io/badge/days%20completed-9-red)
 
 https://adventofcode.com/2023/leaderboard/private/view/3415527
