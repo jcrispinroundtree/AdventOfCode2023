6d5a904572eb9efeb27e2bdadab01ad87f0dca98
jcrispinroundtree <crispinroundtree@gmail.com>
2023-12-16 14:55:38 +0000

    Day 10 Part 1 Complete!

--- Day10.rb
+++ Day10.rb
@@ -1,203 +1,80 @@
-input = File.readlines('Day10-input.txt')
-height, width = input.length, input[0].length
-matrix = Array.new(height) { Array.new(width) }
-input.each_with_index do |line, i|
-  line.each_char.with_index do |char, j|
-    matrix[i][j] = char.to_s
+require 'matrix'
+
+def find_start(matrix)
+  height = matrix.length
+  width = matrix[0].length
+  start = nil
+  direction = nil
+  height.times do |y|
+    width.times do |x|
+      if matrix[y][x] == 'S'
+        start = Vector[y, x]
+          if %w[7 | F].include?(matrix[y - 1][x])
+            direction = 'north'
+          elsif %w[F - L].include?(matrix[y][x - 1])
+            direction = 'west'
+          elsif %w[J - 7].include?(matrix[y][x + 1])
+            direction = 'east'
+          elsif %w[L | J].include?(matrix[y + 1][x])
+            direction = 'south'
+          end
+        break
+      end
+    end
+    break if start
   end
+  [start, direction]
 end
 
-height.times do |y|
-  width.times do |x|
-    if matrix[y][x] == ("S")
-      puts "Found S at #{x+1}, #{y+1}"
-      S_x = x
-      S_y = y
-      break
+def traverse(point, direction, matrix)
+  case direction
+  when 'north'
+    point += Vector[-1, 0]
+    case matrix[point[0]][point[1]]
+    when '7'
+      direction = 'west'
+    when 'F'
+      direction = 'east'
+    end
+  when 'west'
+    point += Vector[0, -1]
+    case matrix[point[0]][point[1]]
+    when 'L'
+      direction = 'north'
+    when 'F'
+      direction = 'south'
+    end
+  when 'east'
+    point += Vector[0, 1]
+    case matrix[point[0]][point[1]]
+    when 'J'
+      direction = 'north'
+    when '7'
+      direction = 'south'
+    end
+  when 'south'
+    point += Vector[1, 0]
+    case matrix[point[0]][point[1]]
+    when 'L'
+      direction = 'east'
+    when 'J'
+      direction = 'west'
     end
   end
+  [point, direction]
 end
 
-x = S_x
-y = S_y
-
-puts matrix[y][x]
-
-north = matrix[y-1][x]
-south = matrix[y+1][x]
-west = matrix[y][x-1]
-east = matrix[y][x+1]
-
-count = 0
-
-def pathfinder_N(matrix, x, y, count)
-  north = matrix[y-1][x]
-  if matrix[y][x] != "S"
-    case north 
-    when "|" 
-      y -= 1
-      count += 1
-      pathfinder_N(matrix, x, y, count)
-    when "F" 
-      y -= 1
-      x += 1
-      count += 2
-      pathfinder_E(matrix, x, y, count)
-    when "7" 
-      y -= 1
-      x -= 1
-      count += 2
-      pathfinder_W(matrix, x, y, count)
-    end
+def main
+  input = File.readlines('Day10-input.txt').map(&:chomp)
+  start, direction = find_start(input)
+  point = start
+  loop_count = 0
+  loop do
+    point, direction = traverse(point, direction, input)
+    loop_count += 1
+    break if point == start && loop_count > 1
   end
-  puts count
+  puts "Loop size: #{loop_count / 2}"
 end
 
-def pathfinder_S(matrix, x, y, count)
-  south = matrix[y+1][x]
-  if matrix[y][x] != "S"
-    case south 
-    when "|" 
-      y += 1
-      count += 1
-      pathfinder_S(matrix, x, y, count)
-    when "J" 
-      y += 1
-      x -= 1
-      count += 2
-      pathfinder_W(matrix, x, y, count)
-    when "L" 
-      y += 1
-      x += 1
-      count += 2 
-      pathfinder_E(matrix, x, y, count)
-    end
-  end
-  puts count
-end
-
-def pathfinder_W(matrix, x, y, count)
-  west = matrix[y][x-1]
-  if matrix[y][x] != "S"
-    case west 
-    when "-" 
-      x -= 1
-      count += 1
-      pathfinder_W(matrix, x, y, count)
-    when "L" 
-      x -= 1
-      y -= 1
-      count += 2
-      pathfinder_N(matrix, x, y, count)
-    when "F" 
-      x -= 1
-      y += 1
-      count += 2
-      pathfinder_S(matrix, x, y, count)
-    end
-  end
-  puts count
-end
-
-def pathfinder_E(matrix, x, y, count)
-  east = matrix[y][x+1]
-  if matrix[y][x] != "S"
-    case east 
-    when "-" 
-      x += 1
-      count += 1
-      pathfinder_E(matrix, x, y, count)
-    when "7" 
-      x += 1
-      y += 1
-      count += 2
-      pathfinder_S(matrix, x, y, count)
-    when "J" 
-      x += 1
-      y -= 1
-      count += 2
-      pathfinder_N(matrix, x, y, count)
-    end
-  end
-  puts count
-end
-
-if north == ("|") || north == ("F") || north == ("7")
-  case north
-  when "|"
-    y -= 1
-    count += 1 
-    pathfinder_N(matrix, x, y, count)
-  when "F"
-    y -= 1
-    x += 1
-    count += 2
-    pathfinder_E(matrix, x, y, count)
-  when "7"
-    y -= 1
-    x -= 1
-    count += 2
-    pathfinder_W(matrix, x, y, count)
-  end
-end
-
-if south == ("|") || south == ("J") || south == ("L")
-  case south
-  when "|"
-    y += 1
-    count += 1
-    pathfinder_S(matrix, x, y, count)
-  when "J" 
-    y += 1
-    x -= 1
-    count += 2
-    pathfinder_W(matrix, x, y, count)
-  when "L" 
-    y += 1
-    x += 1
-    count += 2
-    pathfinder_E(matrix, x, y, count)
-  end
-end
-
-if west == ("-") || west == ("L") || west == ("F")
-  case west
-  when "-" 
-    x -= 1
-    count += 1
-    pathfinder_W(matrix, x, y, count)
-  when "L" 
-    x -= 1
-    y -= 1
-    count += 2
-    pathfinder_N(matrix, x, y, count)
-  when "F" 
-    x -= 1
-    y += 1
-    count += 2 
-    pathfinder_S(matrix, x, y, count)
-  end
-end
-
-if east == ("-") || east == ("J") || east == ("7")
-  case east
-  when "-" 
-    x += 1
-    count += 1
-    pathfinder_E(matrix, x, y, count)
-  when "7" 
-    x += 1
-    y += 1
-    count += 2
-    pathfinder_S(matrix, x, y, count)
-  when "J" 
-    x += 1
-    y -= 1
-    count += 2
-    pathfinder_N(matrix, x, y, count)
-  end
-end   
-
-# Instructions = struct.new(:prev_direction, :current_pipe, :next_direction)
-
-
+main
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # AdventOfCode2023
 Practicing Ruby
 
-![](https://img.shields.io/badge/stars%20⭐-18-yellow)
+![](https://img.shields.io/badge/stars%20⭐-19-yellow)
 ![](https://img.shields.io/badge/days%20completed-9-red)
 
 https://adventofcode.com/2023/leaderboard/private/view/3415527
