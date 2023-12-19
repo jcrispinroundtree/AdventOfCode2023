6f640037eb0dcde7e2bb846a045d30a1abd5f85c
jcrispinroundtree <crispinroundtree@gmail.com>
2023-12-19 11:45:38 +0000

    Day 11 Part 2 Complete!

--- Day11.rb
+++ Day11.rb
@@ -35,5 +35,29 @@ while galaxy_coordinates.length > 1 do
   galaxy_coordinates.shift
 end
 
-puts sum
+puts "Part 1: " + sum.to_s
     
+
+columns = lines.transpose
+total_rows = lines.length + lines.count { |row| !row.include?('#') } * 999999
+total_columns = columns.length + columns.count { |col| !col.include?('#') } * 999999
+
+galaxy_coordinates = []
+lines.each_with_index do |row, row_index|
+  row_offset = row_index + row_index.downto(0).count { |i| !lines[i].include?('#') } * 999999
+  row.each_with_index do |char, col_index|
+    col_offset = col_index + col_index.downto(0).count { |i| !columns[i].include?('#') } * 999999
+    galaxy_coordinates << [col_offset, row_offset] if char == '#'
+  end
+end
+
+sum2 = 0
+while galaxy_coordinates.length > 1 do
+  g1 = galaxy_coordinates[0]
+  galaxy_coordinates[1..].each do |g2|
+    sum2 += (g2[0] - g1[0]).abs + (g2[1] - g1[1]).abs
+  end
+  galaxy_coordinates.shift
+end
+
+puts "Part 2: " + sum2.to_s
