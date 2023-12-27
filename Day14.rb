03e4d84ef94c6dd2ccd7b341a5d11fb721055413
jcrispinroundtree <crispinroundtree@gmail.com>
2023-12-27 20:48:12 +0000

    Day 14 WIP

--- Day14.rb
+++ Day14.rb
@@ -42,11 +42,15 @@ def read_input(file_path)
 end
 
 def tilt_north(grid)
-  (1...grid.length).each do |row|
+  (1...grid.length).to_a.reverse.each do |row|
     (0...grid[row].length).each do |col|
-      if grid[row][col] == 'O' && grid[row - 1][col] == '.'
-        grid[row][col], grid[row - 1][col] = '.', 'O'
+      next unless grid[row][col] == 'O'
+      new_row = row - 1
+      while new_row >= 0 && grid[new_row][col] == '.'
+        new_row -= 1
       end
+      next if new_row == row - 1
+      grid[row][col], grid[new_row + 1][col] = '.', 'O'
     end
   end
   grid
@@ -54,21 +58,29 @@ end
 
 def tilt_west(grid)
   grid.each do |row|
-    (1...row.length).each do |col|
-      if row[col] == 'O' && row[col - 1] == '.'
-        row[col], row[col - 1] = '.', 'O'
+    (1...row.length).to_a.reverse.each do |col|
+      next unless row[col] == 'O'
+      new_col = col - 1
+      while new_col >= 0 && row[new_col] == '.'
+        new_col -= 1
       end
+      next if new_col == col - 1
+      row[col], row[new_col + 1] = '.', 'O'
     end
   end
   grid
 end
 
 def tilt_south(grid)
-  (0...(grid.length - 1)).to_a.reverse.each do |row|
+  (0...(grid.length - 1)).each do |row|
     (0...grid[row].length).each do |col|
-      if grid[row][col] == 'O' && grid[row + 1][col] == '.'
-        grid[row][col], grid[row + 1][col] = '.', 'O'
+      next unless grid[row][col] == 'O'
+      new_row = row + 1
+      while new_row < grid.length && grid[new_row][col] == '.'
+        new_row += 1
       end
+      next if new_row == row + 1
+      grid[row][col], grid[new_row - 1][col] = '.', 'O'
     end
   end
   grid
@@ -76,10 +88,14 @@ end
 
 def tilt_east(grid)
   grid.each do |row|
-    (0...(row.length - 1)).to_a.reverse.each do |col|
-      if row[col] == 'O' && row[col + 1] == '.'
-        row[col], row[col + 1] = '.', 'O'
+    (0...(row.length - 1)).each do |col|
+      next unless row[col] == 'O'
+      new_col = col + 1
+      while new_col < row.length && row[new_col] == '.'
+        new_col += 1
       end
+      next if new_col == col + 1
+      row[col], row[new_col - 1] = '.', 'O'
     end
   end
   grid
