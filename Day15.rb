5756ea710c8a082f19534ec4dc098f99cf61e30b
jcrispinroundtree <crispinroundtree@gmail.com>
2024-01-01 17:26:28 +0000

    Day 15 Part 1 Complete!

--- Day15.rb
+++ Day15.rb
@@ -1,3 +1,14 @@
-# for each item seperated by a , for each character, find ascii value, times by 17, % by 265 and add to current value
-
 input = File.read("Day15-input.txt")
+total = 0
+input.split(',').each do |item|
+  current_value = 0
+  item.each_char do |char|
+    ascii_code = char.ord
+    current_value += ascii_code
+    current_value *= 17
+    current_value %= 256
+  end
+  total += current_value
+end
+
+puts "Total: #{total}"
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # AdventOfCode2023
 Practicing Ruby
 
-![](https://img.shields.io/badge/stars%20⭐-28-yellow)
+![](https://img.shields.io/badge/stars%20⭐-29-yellow)
 ![](https://img.shields.io/badge/days%20completed-14-red)
 
 https://adventofcode.com/2023/leaderboard/private/view/3415527
