84f289ba31a3f20d29b2e9d795725214ddadb922
jcrispinroundtree <crispinroundtree@gmail.com>
2024-01-02 19:32:31 +0000

    Day 15 WIP

--- Day15.rb
+++ Day15.rb
@@ -1,3 +1,4 @@
+# PART 1
 input = File.read("Day15-input.txt")
 total = 0
 input.split(',').each do |item|
@@ -12,3 +13,17 @@ input.split(',').each do |item|
 end
 
 puts "Total: #{total}"
+
+# PART 2
+
+
+# make empty array
+# for each item in input separated by a , 
+#   run hash algorithm on item to determine which box it is talking about
+
+#   if item contains = and the first characters found in the item before the = is not in the array, 
+#     add item to the array replacing the = with a blank space,
+#   otherwise if the first characters found in the item before the = is in the array 
+#     then update the value after the whitespace that is in the array to be the value after the whitespace
+
+#  if item contains - then find the item containing the characters found before the - and delete that item from the array
