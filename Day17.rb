3b1a94f6b0e95bfdabba56553026e0c8d430df73
jcrispinroundtree <crispinroundtree@gmail.com>
2024-01-11 17:04:28 +0000

    Day 17 WIP

---
+++ Day17.rb
@@ -0,0 +1,3 @@
+# pathfinding algorithm, in the directions north south east and west find the lowest number and move in that direction, 
+# however you cannot travel in the same direction for more than 3 blocks, 
+# at this point it needs to move in a different direction
